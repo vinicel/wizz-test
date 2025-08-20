7645319a44d094292d8140e312dacc3a2cbe1b83
Guillaume cornet <guillaudev.dev@gmail.com>
2025-08-20 16:26:02 +0200

    feat: create search api route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require('sequelize');
 const db = require('./models');
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/static`));
 
 app.get('/api/games', (req, res) => db.Game.findAll()
-  .then(games => res.send(games))
+  .then((games) => res.send(games))
   .catch((err) => {
     console.log('There was an error querying games', JSON.stringify(err));
     return res.send(err);
@@ -17,7 +18,7 @@ app.get('/api/games', (req, res) => db.Game.findAll()
 app.post('/api/games', (req, res) => {
   const { publisherId, name, platform, storeId, bundleId, appVersion, isPublished } = req.body;
   return db.Game.create({ publisherId, name, platform, storeId, bundleId, appVersion, isPublished })
-    .then(game => res.send(game))
+    .then((game) => res.send(game))
     .catch((err) => {
       console.log('***There was an error creating a game', JSON.stringify(err));
       return res.status(400).send(err);
@@ -28,7 +29,7 @@ app.delete('/api/games/:id', (req, res) => {
   // eslint-disable-next-line radix
   const id = parseInt(req.params.id);
   return db.Game.findByPk(id)
-    .then(game => game.destroy({ force: true }))
+    .then((game) => game.destroy({ force: true }))
     .then(() => res.send({ id }))
     .catch((err) => {
       console.log('***Error deleting game', JSON.stringify(err));
@@ -51,6 +52,26 @@ app.put('/api/games/:id', (req, res) => {
     });
 });
 
+app.post('/api/games/search', (req, res) => {
+  const { name, platform } = req.body;
+  if (!name && !platform) {
+    return db.Game.findAll();
+  }
+
+  return db.game.findAll({
+    where: {
+      name: {
+        [Op.like]: `%${name}%`,
+      },
+      ...(platform && { platform }),
+    },
+  })
+    .then((game) => res.send(game))
+    .catch((err) => {
+      console.log('***There was an error search a game', JSON.stringify(err));
+      return res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
